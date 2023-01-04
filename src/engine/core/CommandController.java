e725449d4cd744a60db7b2f6241a31b3f4da6abe
HappyTimesHank <HenryArlen@gmail.com>
2023-01-03 22:39:00 -0600

    Update CommandController.java

--- src/engine/core/CommandController.java
+++ src/engine/core/CommandController.java
@@ -1,5 +1,6 @@
 package engine.core;
 
+import engine.events.MouseManager;
 import engine.structure.Entity;
 
 public class CommandController {
@@ -15,7 +16,10 @@ public class CommandController {
 
     // Called every frame
     public void update() {
-
+        // Selects unit if new click, Not Mouse Held
+        if(MouseManager.newClick()){
+            //currentEntityId = GraphicsRuntime.frameBuffer.sampleId();
+        }
     }
     // Called on last/closing frame
     public void end() {
