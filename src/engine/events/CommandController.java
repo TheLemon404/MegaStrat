71e4e04c6a556daf149f1ce3eb6239d700cb6fb2
HappyTimesHank <HenryArlen@gmail.com>
2023-01-03 19:51:09 -0600

    CommandControllerSetup

--- src/engine/events/CommandController.java
+++ src/engine/events/CommandController.java
@@ -1,4 +1,24 @@
 package engine.events;
 
+import engine.structure.Entity;
+
 public class CommandController {
+    public static boolean isItemSelected = false;
+    // ^--- Tells if unit is selected
+    public static Entity itemSelected;
+    // ^--- Last item that was selected
+
+    // Called on first frame
+    public void start() {
+
+    }
+
+    // Called every frame
+    public void update() {
+
+    }
+    // Called on last/closing frame
+    public void end() {
+
+    }
 }
